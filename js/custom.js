7a323420ce5e41f882397fcbe959c10c276aca37
Lesia Lametta <lesialametta@gmail.com>
2017-10-23 23:53:55 +0100

    Full implementation of Div Class Tab-wrap
    Implementation of JS code+all the styling

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
 
 	var hf = function(){
 		var h_header = $('header').height();
@@ -11,8 +11,15 @@ $(function(){
 
 	$(window).bind('load resize', hf);
 
+});*/
+
+$(function() {
+    $('.tabs a').click(function() {
+        $(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+        $(this).parent().siblings().removeClass('active');
+        var id = $(this).attr('href');
+        $(id).removeClass('hide');
+        $(this).parent().addClass('active');
+        return false
+    });
 });
-
-   
-
-
